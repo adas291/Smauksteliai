efc2719bef2ca9d80779e62d69f75ce8708a7d92
Waspirious <kestazzxc@gmail.com>
2023-03-08 11:57:29 +0200

    merged with arminas code

--- Website/User/ViewUsers.php
+++ Website/User/ViewUsers.php
@@ -6,7 +6,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>ViewUsersPage</title>
-        <link rel="stylesheet" href="../styles.css">
+        <link rel="stylesheet" href="../styles.css?v=<?php echo time(); ?>">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	</head>
 	<body>
         <nav>
@@ -17,32 +18,55 @@
             </ul>
         </nav>
         </nav>
+        <input type="text" id="searchBar" placeholder="Search...">
         <table class="table">
-        <?php
-                $sql = "SELECT fname, surname, birth_day, phone_number, country, email, city, SEX.name as 'sex', fk_ROLE_name, CLIENT.name as 'client' FROM MEMBER
-                JOIN SEX on MEMBER.sex = SEX.id_SEX
-                LEFT JOIN CLIENT on MEMBER.fk_CLIENT_id = CLIENT.id";
+        <thead>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>Birth date</th>
+                <th>Phone number</th>
+                <th>Email</th>
+                <th>Country</th>
+                <th>City</th>
+                <th>Sex</th>
+                <th>Role</th>
+                <th>Client</th>
+            </thead>
+            <tbody id="body">
+                <?php
+                        $sql = "SELECT fname, surname, birth_day, phone_number, country, email, city, SEX.name as 'sex', fk_ROLE_name, CLIENT.name as 'client' FROM MEMBER
+                        JOIN SEX on MEMBER.sex = SEX.id_SEX
+                        LEFT JOIN CLIENT on MEMBER.fk_CLIENT_id = CLIENT.id";
 
-                $result = $conn->query($sql);
+                        $result = $conn->query($sql);
 
-                echo "<tr><th>Name</th><th>Surname</th><th>Birth date</th><th>Phone number</th><th>Email</th><th>Country</th><th>City</th><th>Sex</th><th>Role</th><th>Client</th></tr>";
+                        while ($row = mysqli_fetch_assoc($result)) {
 
-                while ($row = mysqli_fetch_assoc($result)) {
-
-                    if ($row['client'])
-                    {
-    
-                        echo "<tr><td>".$row["fname"]."</td><td>".$row["surname"]."</td><td>".$row["birth_day"]."</td><td>".$row["phone_number"]."</td><td>".$row["email"]."</td><td>".$row["country"]."</td><td>"
-                              .$row["city"]."</td><td>".$row["sex"]."</td><td>".$row["fk_ROLE_name"]."</td><td>".$row["client"]."</td></tr>";
-                    }
-                    
-                    else
-                    {
-                        echo "<tr><td>".$row["fname"]."</td><td>".$row["surname"]."</td><td>".$row["birth_day"]."</td><td>".$row["phone_number"]."</td><td>".$row["email"]."</td><td>".$row["country"]."</td><td>"
-                              .$row["city"]."</td><td>".$row["sex"]."</td><td>".$row["fk_ROLE_name"]."</td><td></td></tr>";
-                    }
-                }
-        ?>
+                            if ($row['client'])
+                            {
+            
+                                echo "<tr><td>".$row["fname"]."</td><td>".$row["surname"]."</td><td>".$row["birth_day"]."</td><td>".$row["phone_number"]."</td><td>".$row["email"]."</td><td>".$row["country"]."</td><td>"
+                                    .$row["city"]."</td><td>".$row["sex"]."</td><td>".$row["fk_ROLE_name"]."</td><td>".$row["client"]."</td></tr>";
+                            }
+                            
+                            else
+                            {
+                                echo "<tr><td>".$row["fname"]."</td><td>".$row["surname"]."</td><td>".$row["birth_day"]."</td><td>".$row["phone_number"]."</td><td>".$row["email"]."</td><td>".$row["country"]."</td><td>"
+                                    .$row["city"]."</td><td>".$row["sex"]."</td><td>".$row["fk_ROLE_name"]."</td><td></td></tr>";
+                            }
+                        }
+                ?>
+            </tbody>
         </table>
+        <script>
+            $(document).ready(function(){
+                $("#searchBar").on("keyup",function(){
+                  var value= $(this).val().toLowerCase();
+                  $("#body tr").filter(function(){
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+                  });
+                });
+            });
+        </script>
     </body>
 </html>
