f2d3d91b481af06b17c482087e194b2f5577fc6a
Waspirious <kestazzxc@gmail.com>
2023-03-11 13:33:24 +0200

    trying something with search dropbox

--- Website/Home.php
+++ Website/Home.php
@@ -7,6 +7,9 @@
 		<title>HomePage</title>
         <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
         <link href="http://fonts.cdnfonts.com/css/minecraftia" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+        
 	</head>
 	<body>
         <nav>
@@ -21,5 +24,24 @@
             </ul>
         </nav>
         <button class="btn-minecraft">Press</button>
+        <br><br>
+        <div class="select">
+            <select class="w-50" data-live-search="true">
+                <option>web page</option>
+                <option>web notpage</option>
+                <option>wiki</option>
+                <option>page</option>
+            </select>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+        <script>
+                $(document).ready(function(){
+                    $('select').selectpicker();
+                });
+        </script>
 	</body>
 </html>
