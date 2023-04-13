1ead0df531346b6842fe55940c559cdd12356dd6
adas291 <adomas.poniskaitis@gmail.com>
2023-04-13 12:43:16 +0300

    removing projects from lists now works

--- Website/Project/RemoveProject.php
+++ Website/Project/RemoveProject.php
@@ -1,4 +1,26 @@
 <?php
     include '../Includes/Connect.php';
     $id = $_GET['id'];
+
+
+    // $query1 = 
+    // "DELETE FROM ACADEMIC_GROUP WHERE id = (SELECT `fk_ACADEMIC_GROUP_id` FROM `PROJECT` WHERE id = $id);";
+
+    $query2 = "DELETE FROM `PROJECT` WHERE id = $id;";
+    
+    $conn->begin_transaction();
+
+    // $result1 = $conn->query($query1);
+    $result2 = $conn->query($query2);
+
+    if($result1) {
+        echo "Error: " . $conn->error;
+        $conn->rollback();
+    }
+    else {
+        $conn->commit();
+    }
+
+    $conn->close();
+    header("Location: ../Project/ViewProjects.php?status=RemoveSuccess");
 ?>
